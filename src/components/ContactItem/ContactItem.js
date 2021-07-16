cf8ceecedf9fc3a75ba1a56021ca92a08935f5d3
Nina Vasilieva <aninok92@gmail.com>
2021-07-16 22:02:29 +0300

    добавляет иконку человечка

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
+import { BsFillPersonFill } from "react-icons/bs";
 import s from "./ContactItem.module.scss";
 
 const ContactItem = ({ name, number }) => (
   <div className={s.wrapper}>
+    <BsFillPersonFill size="28" fill="#87CEEB" />
     <p className={s.contact}>{name}</p>
     <p className={s.contact}>{number}</p>
   </div>
