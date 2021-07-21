4a2e16b24d39aced4209425c8bfdab77ea99b363
Nina Vasilieva <aninok92@gmail.com>
2021-07-21 11:51:16 +0300

    добавляет хук useEffect для получения данных из локал сторадж

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,15 +9,19 @@ import Section from "../Section/Section";
 
 function App() {
   const [filter, setFilter] = useState("");
-  const [contacts, setContacts] = useState(
-    () =>
-      JSON.parse(window.localStorage.getItem("contacts")) ?? [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ]
-  );
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    setContacts(
+      () =>
+        JSON.parse(window.localStorage.getItem("contacts")) ?? [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ]
+    );
+  }, []);
 
   useEffect(() => {
     window.localStorage.setItem("contacts", JSON.stringify(contacts));
